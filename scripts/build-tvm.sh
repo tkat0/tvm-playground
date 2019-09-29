4e073334e54ebe7e5cdb284dcd75c5792ee0b7f1
Tomohiro KATO <tomohiro.kato@dena.com>
2019-09-30 07:05:17 +0900

    Upgrade tvm

--- scripts/build-tvm.sh
+++ scripts/build-tvm.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-pushd 3rdparty/tvm
-make -j8
+pushd tvm
+mkdir -p build
+cd build
+cmake \
+    -DUSE_LLVM=llvm-config-8 \
+    -DUSE_RPC=ON \
+    -DUSE_SORT=ON \
+    -DUSE_GRAPH_RUNTIME=ON \
+    .. && \
+time make -j10
 popd
